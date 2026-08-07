8d0498145750da63acc95b13f1d9310a1615e258
roxy061 <feel0627546143@gmail.com>
2026-08-07 12:43:38 +0700

    Update week12-tableData.php

--- week12-tableData.php
+++ week12-tableData.php
@@ -16,12 +16,14 @@
     </head>
     <body>
         <h2>ข้อมูลนักเรียน</h2>
+        <a href="add.php">เพิ่มข้อมูลนักเรียน</a>
         <table border="1">
             <tr>
                 <th>ID</th>
                 <th>Name</th>
                 <th>email</th>
                 <th>age</th>
+                <th>Actions</th>
             </tr>
             <?php foreach($students as $student){ ?>
             <tr>
@@ -29,6 +31,10 @@
                 <td><?= $student['name']?></td>
                 <td><?= $student['email']?></td>
                 <td><?= $student['age']?></td>
+                <td>
+                    <a href="edit.php?id=<?= $student['id']?>">แก้ไข</a>
+                    <a href="delete.php?id=<?= $student['id']?>">ลบ</a>
+                </td>
             </tr>
             <?php } ?>
         </table>
