0b10511fb6f60828f91b12fb8acb8df19e162ced
roxy061 <feel0627546143@gmail.com>
2026-08-07 11:42:50 +0700

    Add files via upload

---
+++ connect.php
@@ -0,0 +1,18 @@
+<?php
+// Database connection parameters
+    $host = "localhost";
+    $dbname = "school";
+    $username = "root";
+    $password = "";
+
+    $pdo = new pdo(
+           "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
+            $username,
+            $password
+        );
+
+        $pdo->setAttribute(
+            PDO::ATTR_ERRMODE,
+            PDO::ERRMODE_EXCEPTION
+        );
+?>
